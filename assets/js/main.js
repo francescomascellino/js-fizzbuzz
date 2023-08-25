2981c9c459e48411f0c6218d918f8a273f4ef6d5
francescomascellino <franesco.mascellino@gmail.com>
2023-08-25 14:45:08 +0200

    add css and code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,66 @@
+
+const fizzBuzzGrid = document.getElementById("fizzBuzzGrid");
+
+/* const fizzBlock = `<div
+class="col border rounded-1 d-flex align-items-center justify-content-center fw-bold p-2 m-2 ${fizzBuzzValue}">
+<span>${fizzBuzzValue}</span>
+</div>`; */
+
 for (let i = 1; i <= 100; i++) 
 {
 
+
+
     if (i % 15 === 0) {
-        const FizzBuzzValue = "FizzBuzz";
-        console.log(FizzBuzzValue);
+        const fizzBuzzValue = "FizzBuzz";
+        console.log(fizzBuzzValue);
+
+        const fizzBlock = `<div
+class="col border rounded-1 d-flex align-items-center justify-content-center fw-bold p-2 m-2 ${fizzBuzzValue}">
+<span>${fizzBuzzValue}</span>
+</div>`;
+
+        fizzBuzzGrid.innerHTML += fizzBlock;
+
     } 
 
     else if (i % 3 === 0) {
-        const FizzBuzzValue = "Fizz";
-        console.log(FizzBuzzValue);
+        const fizzBuzzValue = "Fizz";
+        console.log(fizzBuzzValue);
+
+        const fizzBlock = `<div
+        class="col border rounded-1 d-flex align-items-center justify-content-center fw-bold p-2 m-2 ${fizzBuzzValue}">
+        <span>${fizzBuzzValue}</span>
+        </div>`;
+        
+                fizzBuzzGrid.innerHTML += fizzBlock;
+
     }
 
     else if (i % 5 === 0) {
-        const FizzBuzzValue = "Buzz";
-        console.log(FizzBuzzValue);
+        const fizzBuzzValue = "Buzz";
+        console.log(fizzBuzzValue);
+
+        const fizzBlock = `<div
+        class="col border rounded-1 d-flex align-items-center justify-content-center fw-bold p-2 m-2 ${fizzBuzzValue}">
+        <span>${fizzBuzzValue}</span>
+        </div>`;
+
+                fizzBuzzGrid.innerHTML += fizzBlock;
+
     }
 
     else {
-        console.log(i);
+        const fizzBuzzValue = i;
+        console.log(fizzBuzzValue);
+
+        const fizzBlock = `<div
+        class="col border rounded-1 d-flex align-items-center justify-content-center fw-bold p-2 m-2 ${fizzBuzzValue}">
+        <span>${fizzBuzzValue}</span>
+        </div>`;
+        
+                fizzBuzzGrid.innerHTML += fizzBlock;
+
     }
 
 }
