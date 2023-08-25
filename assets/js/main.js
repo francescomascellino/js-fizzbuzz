948fc8a79123d9fb37fdb156d2c89d711a040f45
francescomascellino <franesco.mascellino@gmail.com>
2023-08-25 13:58:52 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+for (let i = 1; i <= 50; i++) 
+{
+
+    if (i % 15 === 0) {
+        const FizzBuzzValue = "FizzBuzz";
+        console.log(FizzBuzzValue);
+    } 
+
+    else if (i % 3 === 0) {
+        const FizzBuzzValue = "Fizz";
+        console.log(FizzBuzzValue);
+    }
+
+    else if (i % 5 === 0) {
+        const FizzBuzzValue = "Buzz";
+        console.log(FizzBuzzValue);
+    }
+
+    else {
+        console.log(i);
+    }
+
+}
